fa58dfbf33599da4f83e8e74b800c135cd5085c9
Ethan Wessel <ejwessel@gmail.com>
2019-08-04 15:50:51 -0700

    update migrations example

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const ContractA = artifacts.require("ContractA");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(ContractA, 141);
 };
